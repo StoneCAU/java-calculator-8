4be64d7673d880e1786f4f18de8896f9c7c59515
Seokwoo <seokwoo1999@naver.com>
2025-10-18 19:59:16 +0900

    feat(Parser): Parser 클래스 구현
    - 커스텀 구분자 구분 메서드 구현
    - 구분자 String 값 리턴 메서드 구현
    - numbers String 값 리턴 메서드 구현

--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -1,14 +1,29 @@
 package calculator;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.regex.Pattern;
 
 public class Parser {
-    private static final List<String> DEFAULT_DELIMITERS = List.of(",", ":");
+    // 커스텀 구분자 형식인지 판단
+    public boolean isCustomDelimiter(String input) {
+        return input.startsWith(Constants.CUSTOM_INDICATOR_START)
+                && input.contains(Constants.CUSTOM_INDICATOR_END);
+    }
 
-    public List<String> parse(String input) {
-        // "[,:]" 형태의 정규식 생성 - 쉼표 또는 콜론으로 분리
-        String regex = "[" + String.join("", DEFAULT_DELIMITERS) + "]";
-        return Arrays.asList(input.split(regex));
+    // 정규표현식 기반 String 값 리턴
+    public String extractDelimiter(String input) {
+        if (!isCustomDelimiter(input)) {
+            return "[" + String.join("", Constants.DEFAULT_DELIMITERS) + "]";
+        }
+
+        int end = input.indexOf(Constants.CUSTOM_INDICATOR_END);
+        String delimiter = input.substring(2, end);
+        return Pattern.quote(delimiter);
+    }
+
+    // numbers String 값 리턴
+    public String extractNumbers(String input) {
+        if (!isCustomDelimiter(input)) return input;
+        int start = input.indexOf(Constants.CUSTOM_INDICATOR_END);
+        return input.substring(start + 2);
     }
 }
