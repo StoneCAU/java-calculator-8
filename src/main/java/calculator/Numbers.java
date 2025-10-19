48d5df3fa547720e8f33b7676975719f343c4e03
Seokwoo <seokwoo1999@naver.com>
2025-10-19 17:51:01 +0900

    feat(Numbers): 토큰 검증 및 추출 기능 구현

--- src/main/java/calculator/Numbers.java
+++ src/main/java/calculator/Numbers.java
@@ -1,5 +1,10 @@
 package calculator;
 
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Numbers {
     private final Delimiter delimiter;
     private final String numbersPart;
@@ -8,6 +13,7 @@ public class Numbers {
         this.delimiter = delimiter;
         this.numbersPart = numbersPart;
         validatePattern();
+        validateTokens();
     }
 
     private void validatePattern() {
@@ -16,4 +22,25 @@ public class Numbers {
         if (!numbersPart.matches(allowedPattern)) throw new IllegalArgumentException();
     }
 
+    private void validateTokens() {
+        List<String> tokens = Arrays.asList(numbersPart.split(delimiter.regex()));
+        for (String token : tokens) {
+            validateIsNumber(token);
+            validateNoLeadingZero(token);
+        }
+    }
+
+    private void validateIsNumber(String token) {
+        if (!token.matches("\\d+")) throw new IllegalArgumentException();
+    }
+
+    private void validateNoLeadingZero(String token) {
+        if (token.startsWith("0")) throw new IllegalArgumentException();
+    }
+
+    public List<BigInteger> toList() {
+        return Arrays.stream(numbersPart.split(delimiter.regex()))
+                .map(BigInteger::new)
+                .collect(Collectors.toList());
+    }
 }
