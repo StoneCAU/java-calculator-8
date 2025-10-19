da22d4fb21d4bc5c0c011e7c1b5a9e296ad62c44
Seokwoo <seokwoo1999@naver.com>
2025-10-19 17:52:08 +0900

    refactor(Parser): Delimiter, Numbers 클래스에 책임 분리

--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -1,43 +1,42 @@
 package calculator;
 
-import java.util.Arrays;
+import java.math.BigInteger;
 import java.util.List;
 
 public class Parser {
-    private static final List<String> DEFAULT_DELIMITERS = List.of(",", ":");
     private static final String CUSTOM_INDICATOR_START = "//";
     private static final String CUSTOM_INDICATOR_END = "\\n";
 
-    // 커스텀 구분자 형식인지 판단
-    public boolean isCustomDelimiter(String input) {
+    public List<BigInteger> parse(String input) {
+        String delimiterPart = extractDelimiter(input);
+        String numbersPart = extractNumbers(input);
+
+        Delimiter delimiter = new Delimiter(delimiterPart);
+        Numbers numbers = new Numbers(delimiter, numbersPart);
+
+        return numbers.toList();
+    }
+
+    private String extractDelimiter(String input) {
+        if (!isCustomDelimiter(input)) return "";
+
+        int end = input.indexOf(CUSTOM_INDICATOR_END);
+        if (end == -1) throw new IllegalArgumentException();
+
+        String delimiterPart = input.substring(2, end);
+        if (delimiterPart.isEmpty()) throw new IllegalArgumentException();
+
+        return delimiterPart;
+    }
+
+    private boolean isCustomDelimiter(String input) {
         return input.startsWith(CUSTOM_INDICATOR_START)
                 && input.contains(CUSTOM_INDICATOR_END);
     }
 
-    // 정규표현식 기반 String 값 리턴
-    public String extractDelimiter(String input) {
-        if (!isCustomDelimiter(input)) {
-            return "[" + String.join("", DEFAULT_DELIMITERS) + "]";
-        }
-
-        int end = input.indexOf(CUSTOM_INDICATOR_END);
-        if (end == -1) throw new IllegalArgumentException(); // input 형식 오류
-
-        String delimiter = input.substring(2, end);
-        if (delimiter.isEmpty()) throw new IllegalArgumentException(); // input 형식 오류
-
-        return delimiter;
-    }
-
-    // numbers String 값 리턴
-    public String extractNumbers(String input) {
+    private String extractNumbers(String input) {
         if (!isCustomDelimiter(input)) return input;
         int start = input.indexOf(CUSTOM_INDICATOR_END);
         return input.substring(start + 2);
-    }
-
-    // 문자열을 지정된 구분자 기준으로 분리하여 리스트로 반환
-    public List<String> split(String delimiter, String numbers) {
-        return Arrays.asList(numbers.split(delimiter));
     }
 }
